d31c5d1ccecb73fda04572bd2e01967356f3776c
Nathaniel May <nathaniel.may@fishtownanalytics.com>
2021-05-17 14:04:12 -0400

    use os paths

--- setup.py
+++ setup.py
@@ -7,15 +7,15 @@ setup(
     version="0.0.1",
     platforms=["any"],
     python_requires=">=3.6",
-    packages=["bindings/python3/tree-sitter-jinja2"],
+    packages=[os.path.abspath("bindings/python3/tree-sitter-jinja2")],
     install_requires=[
         'tree_sitter==0.19.0'
     ],
     ext_modules=[
         Extension(
             "tree-sitter-jinja2",
-            ["./bindings/python3/include/parser.c"],
-            ["./bindings/python3/include"],
+            [os.path.abspath("./bindings/python3/include/parser.c")],
+            [os.path.abspath("./bindings/python3/include")],
             extra_compile_args=(
                 ["-std=c99", "-fPIC"] if system() != "Windows" else None
             ),
